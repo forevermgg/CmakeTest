4eae3df9599e2f117600cda843605e8dbed55eca
centforever <centforever@CentForeverdeMacBook-Pro.local>
2023-11-25 04:08:18 +0800

    test

--- app/src/main/cpp/git_version.h
+++ app/src/main/cpp/git_version.h
@@ -2,4 +2,4 @@
 #include <string>
 
 const std::string GIT_BRANCH = "main";
-const std::string GIT_COMMIT_ID = "0a1c7e4a49bcc01389beec3b16dd614937e4c5d6";
+const std::string GIT_COMMIT_ID = "9ed09d9fd0e2dda91fc9381ff7b7375a00742a44";
--- app/src/main/cpp/version.h
+++ app/src/main/cpp/version.h
@@ -1,3 +1,3 @@
-const char* GIT_COMMIT_ID="0a1c7e4a49bcc01389beec3b16dd614937e4c5d6";
+const char* GIT_COMMIT_ID="9ed09d9fd0e2dda91fc9381ff7b7375a00742a44";
 const char* GIT_TAG="";
 const char* GIT_BRANCH="main";
