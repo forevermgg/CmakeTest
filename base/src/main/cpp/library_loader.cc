d3d4d27394655da562cae3c18076a08e0f5f18d6
centforever <centforever@CentForeverdeMacBook-Pro.local>
2023-11-25 15:11:31 +0800

    test

--- base/src/main/cpp/library_loader.cc
+++ base/src/main/cpp/library_loader.cc
@@ -2,6 +2,7 @@
 #include "jni/jni_util.h"
 #include "log_settings.h"
 #include "logging.h"
+#include "util/event_loop.hpp"
 
 // This is called by the VM when the shared library is first loaded.
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
@@ -11,6 +12,15 @@ JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   // Initialize the Java VM.
   base::jni::InitJavaVM(vm);
   JNIEnv* env = base::jni::AttachCurrentThread();
+  if (base::util::EventLoop::has_implementation()) {
+    BASE_LOG(ERROR) << "has_implementation";
+  } else {
+    BASE_LOG(ERROR) << "not has_implementation";
+  }
+  base::util::EventLoop::main().run_until([&] {
+    BASE_LOG(ERROR) << "base::util::EventLoop::main().run_until";
+    return true;
+  });
   BASE_LOG(ERROR) << "JNI_OnLoad";
   return JNI_VERSION_1_4;
 }
