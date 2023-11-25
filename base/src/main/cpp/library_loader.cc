e894325ba12b851e56ffd401daa961be7b8a0c81
centforever <centforever@CentForeverdeMacBook-Pro.local>
2023-11-25 15:49:18 +0800

    test

--- base/src/main/cpp/library_loader.cc
+++ base/src/main/cpp/library_loader.cc
@@ -3,6 +3,8 @@
 #include "log_settings.h"
 #include "logging.h"
 #include "util/event_loop.hpp"
+#include "util/scope_exit.hpp"
+#include "util/uv/scheduler.hpp"
 
 // This is called by the VM when the shared library is first loaded.
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
@@ -11,6 +13,9 @@ JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   base::SetLogSettings(log_settings);
   // Initialize the Java VM.
   base::jni::InitJavaVM(vm);
+  base::util::Scheduler::set_default_factory([]() {
+    return std::make_shared<base::util::UvMainLoopScheduler>();
+  });
   JNIEnv* env = base::jni::AttachCurrentThread();
   if (base::util::EventLoop::has_implementation()) {
     BASE_LOG(ERROR) << "has_implementation";
@@ -22,5 +27,15 @@ JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
     return true;
   });
   BASE_LOG(ERROR) << "JNI_OnLoad";
+  bool called = false;
+  auto handler = [&]() noexcept
+  {
+      called = true;
+  };
+  {
+    auto seg = base::util::make_scope_exit(handler);
+    BASE_LOG(ERROR) << "called = " << called;
+  }
+  BASE_LOG(ERROR) << "called = " << called;
   return JNI_VERSION_1_4;
 }
