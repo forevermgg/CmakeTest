fc888f6544b2c34f1051d17985ad38c002dbc578
forevermgg <centforever@163.com>
2023-12-29 14:49:27 +0800

    test

--- app/src/main/cpp/git_version.h
+++ app/src/main/cpp/git_version.h
@@ -2,4 +2,4 @@
 #include <string>
 
 const std::string GIT_BRANCH = "main";
-const std::string GIT_COMMIT_ID = "834f56d9f1107e9d4d2e71e4aff234b911fd19bb";
+const std::string GIT_COMMIT_ID = "f066cbb7ed3d4b0379ec959856b9601083aede95";
--- app/src/main/cpp/version.h
+++ app/src/main/cpp/version.h
@@ -1,3 +1,3 @@
-const char* GIT_COMMIT_ID="834f56d9f1107e9d4d2e71e4aff234b911fd19bb";
+const char* GIT_COMMIT_ID="f066cbb7ed3d4b0379ec959856b9601083aede95";
 const char* GIT_TAG="";
 const char* GIT_BRANCH="main";
