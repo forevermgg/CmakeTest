ae775d1eb8fe46589882be82dc952c5f1622d4f2
centforever <centforever@163.com>
2023-12-29 23:42:51 +0800

    test curl

--- app/src/main/cpp/git_version.h
+++ app/src/main/cpp/git_version.h
@@ -2,4 +2,4 @@
 #include <string>
 
 const std::string GIT_BRANCH = "main";
-const std::string GIT_COMMIT_ID = "e26b155a300ab88a093f32d69760184d610d0058";
+const std::string GIT_COMMIT_ID = "606e5533ef1f222b3c02be8277cfcce0070bd96a";
--- app/src/main/cpp/version.h
+++ app/src/main/cpp/version.h
@@ -1,3 +1,3 @@
-const char* GIT_COMMIT_ID="e26b155a300ab88a093f32d69760184d610d0058";
+const char* GIT_COMMIT_ID="606e5533ef1f222b3c02be8277cfcce0070bd96a";
 const char* GIT_TAG="";
 const char* GIT_BRANCH="main";
